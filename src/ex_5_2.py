ed37d5764efa39c169778c8364b888dd8fa58580
sukkanikitha13 <156538908+sukkanikitha13@users.noreply.github.com>
2024-02-28 22:07:11 -0600

    Update 5.2

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -7,6 +7,7 @@ This module contains an entry point that
 - writes the processed data to a file called `ex_5_2-processed.csv`
 """
 import numpy as np
+import csv
 
 try:
     from src.util import get_repository_root
@@ -22,5 +23,14 @@ if __name__ == "__main__":
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
 
     # Complete the data processing steps using numpy here.
+    with open(INFILE,'r') as csv_file:
+        reader = csv.reader(csv_file)
+        data = list(reader)
+        numpy_array = np.array(data, dtype=float)
+        new_array = numpy_array - np.mean(numpy_array)
+
+        std_dev = np.std(new_array)
+        processed = new_array/std_dev
 
     # Save the output to OUTFILE using numpy routines.
+        np.savetxt(OUTFILE, processed)
